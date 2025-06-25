ae33795c4d6bee625f04a90561365bce5d46b3f1
ardittristan <42787508+ardittristan@users.noreply.github.com>
2025-06-25 12:46:40 +0200

    make script optional

--- graficelly-custom.php
+++ graficelly-custom.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Graficelly custom
  * Plugin URI:        https://graficelly.nl
- * Version:           1.3.0
+ * Version:           1.3.1
  * Author:            Graficelly
  * Author URI:        https://graficelly.nl
  * Text Domain:       graficelly-custom
@@ -22,16 +22,11 @@ require_once( __DIR__ . '/tags/_register.php' );
 
 function register_graficelly_frontend_scripts() {
     wp_register_script(
-        'graficelly-elements-handlers',
-        plugins_url( 'assets/js/elements-handlers.js', __FILE__ ),
+        'graficelly-dynamic-media-carousel-handler',
+        plugins_url( 'assets/js/dynamic-media-carousel-handler.js', __FILE__ ),
         [ 'pro-elements-handlers' ],
         false,
         true
     );
 }
 add_action( 'elementor/frontend/before_register_scripts', 'register_graficelly_frontend_scripts', 11 );
-
-function enqueue_graficelly_frontend_scripts() {
-    wp_enqueue_script('graficelly-elements-handlers');
-}
-add_action( 'elementor/frontend/before_enqueue_scripts', 'enqueue_graficelly_frontend_scripts', 11 );
